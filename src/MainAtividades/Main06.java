d32ac3de059db0873029f58a46f66e956d4b3504
Mateus Lemos <mateuslsdm98@gmail.com>
2023-03-31 16:35:05 -0300

    Update

---
+++ src/MainAtividades/Main06.java
@@ -0,0 +1,11 @@
+package MainAtividades;
+
+import Atividades.Exercicio06;
+
+public class Main06 {
+    public static void main(String[] args) {
+        Exercicio06 numeroAS = new Exercicio06(20);
+
+        numeroAS.imprimirAntSuc();
+    }
+}
